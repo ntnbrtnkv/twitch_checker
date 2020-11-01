80a44a1ea99e5b33c5010ec49c5b228230268fbb
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2020-11-01 17:07:55 +0500

    feat: add jobs, clear handlers

--- bot.py
+++ bot.py
@@ -7,15 +7,23 @@ logger = logging.getLogger('telegram-bot')
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 INTERVAL = int(os.getenv('BOT_INTERVAL'))
+THRESHOLD = float(os.getenv('THRESHOLD'))
 
 
 class Bot:
     def __init__(self, out_folder, twitch):
         self.out_folder = out_folder
         self.twitch = twitch
+        self.jobs = {}
 
-    def job_name(self, chat_id, channel, item):
+    def global_job_name(self, chat_id, channel, item):
         return f'{chat_id}:{channel}:{item}'
+
+    def job_name(self, channel, item):
+        return f'{channel}:{item}'
+
+    def parse_job(self, job_name):
+        return job_name.split(':')
 
     def check(self, context):
         job = context.job
@@ -34,42 +42,73 @@ class Bot:
     def watch(self, update, context):
         chat_id = update.message.chat_id
         try:
-            [channel, obj] = context.args
+            [channel, obj] = context.args[:2]
             job_context = [
                 chat_id,
                 channel,
                 obj
             ]
+            threshold = THRESHOLD
+            if len(context.args) == 3:
+                threshold = float(context.args[2])
             context.job_queue.run_repeating(self.check, INTERVAL, context=job_context,
-                                            name=self.job_name(chat_id, channel, obj))
-            self.twitch.add(channel, obj)
-            update.message.reply_text('Job scheduled')
+                                            name=self.global_job_name(chat_id, channel, obj))
+            job_name = self.job_name(channel, obj)
+            if chat_id not in self.jobs:
+                self.jobs[chat_id] = []
+            if job_name in self.jobs[chat_id]:
+                update.message.reply_text(f'Job {job_name} has been scheduled already')
+                return
+            self.jobs[chat_id].append(job_name)
+            self.twitch.add(channel, obj, threshold)
+            update.message.reply_text(f'Job {job_name} scheduled', )
         except (IndexError, ValueError):
-            update.message.reply_text('Usage: /watch <channel> <eoe|bread|axe>')
+            update.message.reply_text('Usage: /watch <channel> <eoe|bread|axe> <optional: threshold>')
 
-    def remove_job_if_exists(self, name, context):
+    def remove_job_if_exists(self, chat_id, channel, obj, context):
         """Remove job with given name. Returns whether job was removed."""
-        current_jobs = context.job_queue.get_jobs_by_name(name)
+        global_name = self.global_job_name(chat_id, channel, obj)
+        job_name = self.job_name(channel, obj)
+        current_jobs = context.job_queue.get_jobs_by_name(global_name)
         if not current_jobs:
-            return False
+            return 'You have no active watchers.'
         for job in current_jobs:
             job.schedule_removal()
-        return True
+        self.jobs[chat_id].remove(job_name)
+        found = False
+        for c in self.jobs:
+            found = found or any(job == job_name for job in self.jobs[c])
+        if not found:
+            self.twitch.remove(channel, obj)
+        return 'Watch successfully cancelled!'
 
     def stop(self, update, context):
         """Remove the job if the user changed their mind."""
         chat_id = update.message.chat_id
         try:
             [channel, obj] = context.args
-            job_removed = self.remove_job_if_exists(self.job_name(chat_id, channel, obj), context)
-            if job_removed:
-                self.twitch.remove(channel, obj)
-                text = 'Watch successfully cancelled!'
-            else:
-                text = 'You have no active watchers.'
-            update.message.reply_text(text)
+            update.message.reply_text(self.remove_job_if_exists(chat_id, channel, obj, context))
         except (IndexError, ValueError):
             update.message.reply_text('Usage: /stop <channel> <eoe|bread|axe>')
+
+    def my_jobs(self, update, context):
+        chat_id = update.message.chat_id
+        if chat_id not in self.jobs or len(self.jobs[chat_id]) == 0:
+            update.message.reply_text('No active jobs')
+        else:
+            update.message.reply_text('\n'.join(self.jobs[chat_id]))
+
+    def clear(self, update, context):
+        chat_id = update.message.chat_id
+        if chat_id not in self.jobs or len(self.jobs[chat_id]) == 0:
+            update.message.reply_text('No active jobs')
+        else:
+            jobs = []
+            for job in self.jobs[chat_id]:
+                [channel, obj] = self.parse_job(job)
+                self.remove_job_if_exists(chat_id, channel, obj, context)
+                jobs.append(job)
+            update.message.reply_text('Removed jobs:\n' + '\n'.join(jobs))
 
     def run(self):
         updater = t.Updater(TOKEN, use_context=True)
@@ -78,6 +117,8 @@ class Bot:
 
         dp.add_handler(t.CommandHandler('watch', self.watch))
         dp.add_handler(t.CommandHandler('stop', self.stop))
+        dp.add_handler(t.CommandHandler('jobs', self.my_jobs))
+        dp.add_handler(t.CommandHandler('clear', self.clear))
 
         logger.info('Bot starting')
 
--- twitch.py
+++ twitch.py
@@ -7,7 +7,6 @@ import cv2
 import numpy as np
 from streamlink import Streamlink
 
-THRESHOLD = float(os.getenv('THRESHOLD'))
 INTERVAL = int(os.getenv('TWITCH_INTERVAL'))
 
 TMP_STREAM_FILE = 'stream.bin'
@@ -59,54 +58,70 @@ class Twitch:
         imgdata = capture.read()[1]
         return imgdata
 
-    def find_template(self, image_data, template_file, result_file):
-        template = cv2.imread(template_file, cv2.IMREAD_UNCHANGED)
-        h, w = template.shape[:2]
+    def find_template(self, image_data, channel):
+        for template in self.registry[channel]:
+            threshold = self.registry[channel][template]['t']
+            logger.debug(f'Finding template {channel}:{template} with threshold {threshold}')
+            template_file = os.path.join(TEMPLATE_FOLDER, f'{template}.png')
+            result_file = os.path.join(self.out_folder, channel, f'{template}.png')
 
-        tm = template[:, :, 0:3]
+            template = cv2.imread(template_file, cv2.IMREAD_UNCHANGED)
+            h, w = template.shape[:2]
 
-        res = cv2.matchTemplate(image_data, tm, cv2.TM_CCORR_NORMED)
+            tm = template[:, :, 0:3]
 
-        loc = np.where(res >= THRESHOLD)
-        result = image_data.copy()
-        found = len(loc[0]) > 0
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(result, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
-            logger.debug(f'{pt}: {res[pt[1]][pt[0]]}')
+            res = cv2.matchTemplate(image_data, tm, cv2.TM_CCORR_NORMED)
 
-        # save results
-        if found:
-            logger.debug(f'Flushing result to file')
-            dir = os.path.dirname(result_file)
-            if not os.path.exists(dir):
-                os.makedirs(dir)
-            cv2.imwrite(result_file, result)
-        else:
-            logger.debug(f'Not found anything')
+            loc = np.where(res >= threshold)
+            result = image_data.copy()
+            found = len(loc[0]) > 0
+            for pt in zip(*loc[::-1]):
+                cv2.rectangle(result, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
+                logger.debug(f'{pt}: {res[pt[1]][pt[0]]}')
 
-    def run_job(self, name, channel, template):
+            # save results
+            if found:
+                logger.debug(f'Flushing result to file')
+                dir = os.path.dirname(result_file)
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                cv2.imwrite(result_file, result)
+            else:
+                logger.debug(f'Not found anything')
+
+    def run_job(self, name, channel):
         logger.info(f'Running job {name}')
         data = self.fetch_frame(channel)
         if data is None:
             logger.debug(f'Stream {channel} is down')
         else:
-            logger.debug(f'Finding template {template}')
-            self.find_template(data, os.path.join(TEMPLATE_FOLDER, f'{template}.png'),
-                               os.path.join(self.out_folder, channel, f'{template}.png'))
+            logger.debug(f'Finding templates at {channel}')
+            self.find_template(data, channel)
 
     def job_name(self, channel, template):
         return f'{channel}:{template}'
 
-    def add(self, channel, template):
+    def add(self, channel, template, threshold):
         name = self.job_name(channel, template)
-        self.registry[name] = SetInterval(INTERVAL, lambda: self.run_job(name, channel, template))
-        logger.debug(f'Job {name} is scheduled')
+        if channel not in self.registry:
+            self.registry[channel] = {}
+        if template not in self.registry[channel]:
+            self.registry[channel][template] = {'t': threshold,
+                                                'ev': SetInterval(INTERVAL, lambda: self.run_job(name, channel))}
+            logger.debug(f'Job {name} is scheduled')
+            return True
+        else:
+            logger.debug(f'Job {name} has been scheduled already')
+            return False
 
     def remove(self, channel, template):
         name = self.job_name(channel, template)
-        if name in self.registry:
-            ev = self.registry[name]
+        if channel in self.registry and template in self.registry[channel]:
+            ev = self.registry[channel][template]['ev']
             ev.cancel()
+            del self.registry[channel][template]
             logger.debug(f'Job {name} is removed')
+            return True
         else:
             logger.debug(f'Job {name} not found')
+            return False
